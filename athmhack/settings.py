ed30f93f062397bb147ceeff49fca6c755696174
GabrielRivera21 <gabriel.rivera2192@gmail.com>
2015-09-19 15:01:49 -0400

    Added Virtualenv variables

--- athmhack/settings.py
+++ athmhack/settings.py
@@ -1,83 +1,162 @@
 """
 Django settings for athmhack project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+from configurations import Configuration, values
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+class Common(Configuration):
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e)^u@h8y=3vl87%88h3ka5ceh_v=93--9od7tzi!k9dpvng9e1'
+    ENVIRONMENT = values.Value(environ_prefix=None, default='DEVELOPMENT')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = values.SecretValue()
 
-TEMPLATE_DEBUG = True
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = values.BooleanValue(False)
 
-ALLOWED_HOSTS = []
+    TEMPLATE_DEBUG = values.BooleanValue(DEBUG)
 
+    ALLOWED_HOSTS = []
 
-# Application definition
+    # Application definition
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.staticfiles',
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
+        # Third party
+        'corsheaders',
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+        # Apps
+    )
 
-ROOT_URLCONF = 'athmhack.urls'
+    MIDDLEWARE_CLASSES = (
+        'djangosecure.middleware.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-WSGI_APPLICATION = 'athmhack.wsgi.application'
+    ROOT_URLCONF = 'athmhack.urls'
 
+    WSGI_APPLICATION = 'athmhack.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+    # Database
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+    DATABASES = values.DatabaseURLValue(
+        'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.7/topics/i18n/
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    DATE_INPUT_FORMATS = ('%m/%d/%Y', '%Y-%m-%d',)
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.7/howto/static-files/
+    MEDIA_ROOT = 'media'
+    MEDIA_URL = '/media/'
+
+    STATIC_URL = '/static/'
+
+    TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
+    CORS_ORIGIN_ALLOW_ALL = True
+
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+    DOMAIN = values.Value(environ_prefix=None)
 
-LANGUAGE_CODE = 'en-us'
+    SITE_NAME = values.Value(environ_prefix=None)
 
-TIME_ZONE = 'UTC'
+    ACTIVATION_URL = values.Value(environ_prefix=None)
 
-USE_I18N = True
+    PASSWORD_RESET_CONFIRM_URL = values.Value(environ_prefix=None)
 
-USE_L10N = True
+    DEFAULT_FROM_EMAIL = values.Value()
+    EMAIL_HOST = values.Value()
+    EMAIL_HOST_USER = values.Value()
+    EMAIL_HOST_PASSWORD = values.Value()
+    EMAIL_PORT = values.IntegerValue()
+    EMAIL_USE_TLS = values.BooleanValue(False)
 
-USE_TZ = True
+
+class Development(Common):
+    """
+    The in-development settings and the default configuration.
+    """
+    DEBUG = True
+
+    TEMPLATE_DEBUG = True
+
+    ALLOWED_HOSTS = []
+
+    INSTALLED_APPS = Common.INSTALLED_APPS + (
+        'debug_toolbar',
+    )
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    DEBUG_TOOLBAR_PATCH_SETTINGS = values.BooleanValue(True)
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+class Staging(Common):
+    """
+    The in-staging settings.
+    """
+    INSTALLED_APPS = Common.INSTALLED_APPS + (
+        'djangosecure',
+        'storages',
+    )
 
-STATIC_URL = '/static/'
+    # django-secure
+    SESSION_COOKIE_SECURE = values.BooleanValue(True)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_FRAME_DENY = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SECURE_PROXY_SSL_HEADER = values.TupleValue(
+        ('HTTP_X_FORWARDED_PROTO', 'https')
+    )
+
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    PROTOCOL = 'https'
+
+
+class Production(Staging):
+    """
+    The in-production settings.
+    """
+    pass
--- athmhack/wsgi.py
+++ athmhack/wsgi.py
@@ -1,14 +1,33 @@
 """
 WSGI config for athmhack project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "athmhack.settings")
+import dotenv
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+try:
+    dotenv.read_dotenv(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except Exception as e:
+    print(e)
+
+
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+if ENVIRONMENT == 'STAGING':
+    settings = 'staging'
+elif ENVIRONMENT == 'PRODUCTION':
+    settings = 'production'
+else:
+    settings = 'development'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'athmhack.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', settings.title())
+
+from configurations.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
